cd500949283db2abb316e7631a95f27cb886c51a
k890m <94244306+k890m@users.noreply.github.com>
2023-12-25 19:44:24 -0500

    React Framework + WelcomePage
    cerate-react-app and created Welcome Page component.

---
+++ client/src/components/WelcomePage.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const WelcomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to a Fresh Start 🌟</h1>
+      <p>Embrace the New Year with Purpose and Possibility.</p>
+      <p>Let's craft your roadmap to success together!</p>
+
+      <button>Start Planning</button>
+    </div>
+  );
+};
+
+export default WelcomePage;
